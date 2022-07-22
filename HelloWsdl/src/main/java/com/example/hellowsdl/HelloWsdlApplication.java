c1b7e9a9601852095640c8fd3406e80c536f09c5
marcoguerrieri <marco.guerrieri@liparipeople.com>
2022-07-22 16:16:54 +0200

    Prova

---
+++ HelloWsdl/src/main/java/com/example/hellowsdl/HelloWsdlApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellowsdl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWsdlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWsdlApplication.class, args);
+    }
+
+}
---
+++ HelloWsdl/src/test/java/com/example/hellowsdl/HelloWsdlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellowsdl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWsdlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
