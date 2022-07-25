0f57f07d91cd650916ee42a6717b8e52c56dc9af
stefanohid <stefanohidalgo35@gmail.com>
2022-07-25 11:00:47 +0200

    Primo push da IntelliJ

--- ExerciseTest.java
+++ ExerciseTest.java
@@ -4,6 +4,7 @@ public class ExerciseTest {
 	public static void main(String[] args) {
 		System.out.println("Ciao Marco!");
 		System.out.println("Ciao stefano!");
+		System.out.println("Ciao da IntelliJ");
 	}
 
 }
